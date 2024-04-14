d064e7646135f064aaca003a30cf193a888c07c2
kevin <kelvnjogu@gmail.com>
2024-04-14 19:01:51 +0300

    Initial Commit

---
+++ CONFIG.py
@@ -0,0 +1,7 @@
+# Here we have: the bot’s token and the time zone it will operate in (you’ll need this in the future to specify the
+# time for message updating. Telegram API doesn\’t allow you to find out a user’s time zone, therefore the update time
+# should be displayed with a time zone prompt).
+
+TOKEN = '7120568382:AAG8ua_NYujNEBVRtURHsQ15FYua6qESrmI'
+# TIMEZONE = 'EAT'
+# TIMEZONE_COMMON_NAME = 'Nairobi'
--- main.py
+++ main.py
@@ -1,16 +1,92 @@
-# This is a sample Python script.
+import telebot
+import CONFIG
+from telebot.types import Message
+from currencydata import search_currency, trade_check
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Creating the bot
+bot = telebot.TeleBot(CONFIG.TOKEN, parse_mode=None)
+print('bot started')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# defining a message handler which handles incoming /start and /help commands
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(chat_id=message.chat.id,
+                     text='Good Day Trader 👋,\n\n'
+                          'Welcome!! 🤝\n\n'
+                          'I will get you the current exchange rates.\n\n'
+                          'Type /exchange to get today\'s USD/KES, EUR/KES, GBP/KES & AUD/KES rates\n\n'
+                          'Type /currency to get today\'s rate for any other currency in the world\n\n'
+                          'Type /trading to get advice on whether to buy or sell a currency\n\n'
+                          'Type /help for more help'
+                     )
 
 
-# Press the green button in the gutter to run the script.
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.add(telebot.types.InlineKeyboardButton(text='Message Kevin for Help',
+                                                    url='https://www.linkedin.com/in/kevin-njogu-aci-72565087/'))
+    bot.send_message(message.chat.id,
+                     'The available commands are /exchange,/currency,/trading,& /help.\n\n'
+                     'If a command is not working, recheck and ensure you are typing correctly and in lowercase\n\n'
+                     'if you have any other issue click on Message kevin for help and text me',
+                     reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['exchange'])
+def exchange_command(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(telebot.types.InlineKeyboardButton('USD', callback_data='get-USD'),
+                 telebot.types.InlineKeyboardButton('EUR', callback_data='get-EUR'),
+                 telebot.types.InlineKeyboardButton('GBP', callback_data='get-GBP'),
+                 telebot.types.InlineKeyboardButton('AUD', callback_data='get-AUD'))
+    bot.send_message(message.chat.id, 'Click on the Currency of choice:', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def iq_callback(query):
+    data = query.data
+    if data.startswith('get-'):
+        get_ex_callback(query)
+
+
+def get_ex_callback(query):
+    bot.answer_callback_query(query.id)
+    send_exchange_results(query.message, query.data[4:])
+
+
+def send_exchange_results(message, curr_code):
+    bot.send_chat_action(message.chat.id, 'typing')
+    ex = search_currency(curr_code)
+    bot.send_message(message.chat.id,
+                     f'The exchange rate for {curr_code}/KES is: {ex}',
+                     parse_mode='HTML')
+
+
+@bot.message_handler(commands=['currency'])
+def other_currency_command(message):
+    bot.send_message(chat_id=message.chat.id, text='Enter another currency', parse_mode='HTML')
+    bot.register_next_step_handler(message, process_currency_input)
+
+
+def process_currency_input(message: Message):
+    user_input = message.text
+    result = search_currency(user_input.upper())
+    bot.send_message(message.chat.id, result)
+
+
+@bot.message_handler(commands=['trading'])
+def other_currency_command(message):
+    bot.send_message(chat_id=message.chat.id, text='Enter the currency you want to trade', parse_mode='HTML')
+    bot.register_next_step_handler(message, process_trading)
+
+
+def process_trading(message: Message):
+    user_input = message.text
+    result = trade_check(user_input.upper())
+    bot.send_message(message.chat.id, result)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    bot.infinity_polling()
